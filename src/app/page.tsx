9ff6b23dbd82b50ea79338b50d3a23a9fa4f9bc4
Saksham Verma <141804247+SakshamWebTeasor@users.noreply.github.com>
2023-09-15 17:05:03 +0530

    add homepageType

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
-  return (
-    <main >
-      <h1 className="font-display text-5xl font-bold">Hello World</h1>
-    </main>
-  )
+import { Metadata } from "next";
+import { SliceZone } from "@prismicio/react";
+
+import { createClient } from "@/prismicio";
+// import { components } from "@/slices";
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("home_page");
+  return <div className="text-red-500">It Worked</div>
+  // return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("home_page");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
