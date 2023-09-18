af7848ccc274c63c4f256695e932d748001d379d
Saksham Verma <141804247+SakshamWebTeasor@users.noreply.github.com>
2023-09-18 10:52:47 +0530

    header and footer design completed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,24 +2,28 @@ import React from 'react'
 import { createClient } from '@/prismicio'
 import { PrismicNextLink } from '@prismicio/next';
 import Link from 'next/link';
+import Icon from '@/components/newLogo';
+import Bounded from '@/components/Bounded';
 
 async function Footer() {
     const client = createClient();
     const settings = await client.getSingle("settings");
     return (
-        <footer>
+        <Bounded>
+            <div className='flex sm:flex-row flex-col justify-between items-center gap-6'>
             <Link href='/'>
-                {settings.data.site_title}
+                <Icon />
             </Link>
-            <p>©{new Date().getFullYear()} {settings.data.site_title}</p>
-            <ul>
+            <p className='text-sm'>©{new Date().getFullYear()} {settings.data.site_title}</p>
+            <ul className='flex'>
                 {settings.data.navigation.map(({ label, link }) => (
                     <li key={label}>
-                        <PrismicNextLink field={link}>{label}</PrismicNextLink>
+                        <PrismicNextLink field={link} className='p-4'>{label}</PrismicNextLink>
                     </li>
                 ))}
             </ul>
-        </footer>
+            </div>
+        </Bounded>
     )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,25 +2,29 @@ import React from 'react'
 import { createClient } from '@/prismicio'
 import { PrismicNextLink } from '@prismicio/next';
 import Link from 'next/link';
+import Bounded from '@/components/Bounded';
+import Icon from '@/components/newLogo';
 
 async function Header() {
     const client = createClient();
     const settings = await client.getSingle("settings");
     return (
-        <header>
-            <Link href='/'>
-                {settings.data.site_title}
-            </Link>
-            <nav>
-                <ul>
-                    {settings.data.navigation.map(({ label, link }) => (
-                        <li key={label}>
-                            <PrismicNextLink field={link}>{label}</PrismicNextLink>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
+        <Bounded as="header" className='py=4 md:py-6 lg:py-8'>
+            <div className='flex gap-4 items-center justify-between sm:flex-row flex-col'>
+                <Link href='/'>
+                    <Icon />
+                </Link>
+                <nav>
+                    <ul className='flex'>
+                        {settings.data.navigation.map(({ label, link }) => (
+                            <li key={label}>
+                                <PrismicNextLink field={link} className='p-4'>{label}</PrismicNextLink>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+        </Bounded>
     )
 }
 
