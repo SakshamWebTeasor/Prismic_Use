d39680de7640d75964bfea98a846bf113f7f5415
Saksham Verma <141804247+SakshamWebTeasor@users.noreply.github.com>
2023-09-18 17:05:27 +0530

    add prsimic preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter, Nunito, Nunito_Sans } from 'next/font/google'
 import clsx from 'clsx' // for handeling multiple font.class in single className
-import { createClient } from '@/prismicio'
+import { createClient, repositoryName } from '@/prismicio'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import { PrismicPreview } from '@prismicio/next'
 
 // found from https://nextjs.org/docs/app/building-your-application/optimizing/fonts#google-fonts
 
@@ -48,6 +49,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <div className='fixed bg-gradient-to-t from-emerald-50 to-cyan-50 z-[-1] inset-0 opacity-50'></div>
+        <PrismicPreview repositoryName={repositoryName} />
       </body> {/** incase single direclty use => inter.className */}
     </html>
   )
