7860baee644da99047aa7623576cef642024184f
Saksham Verma <sakshamvwebteasor@gmail.com>
2023-12-20 09:56:55 +0530

    SpeedInsight Package added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,56 +1,61 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter, Nunito, Nunito_Sans } from 'next/font/google'
-import clsx from 'clsx' // for handeling multiple font.class in single className
-import { createClient, repositoryName } from '@/prismicio'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
-import { PrismicPreview } from '@prismicio/next'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter, Nunito, Nunito_Sans } from "next/font/google";
+import clsx from "clsx"; // for handeling multiple font.class in single className
+import { createClient, repositoryName } from "@/prismicio";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { PrismicPreview } from "@prismicio/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // found from https://nextjs.org/docs/app/building-your-application/optimizing/fonts#google-fonts
 
 const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-  display: 'swap',
-})
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
 const nunito = Nunito({
-  subsets: ['latin'],
-  variable: '--font-nunito',
-  display: 'swap',
-})
+  subsets: ["latin"],
+  variable: "--font-nunito",
+  display: "swap",
+});
 const nunitoSans = Nunito_Sans({
-  subsets: ['latin'],
-  variable: '--font-nunito-sans',
-  display: 'swap',
-})
+  subsets: ["latin"],
+  variable: "--font-nunito-sans",
+  display: "swap",
+});
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
   const settings = await client.getSingle("settings");
   return {
-    title: settings.data.site_title || 'Prismic Use',
-    description: settings.data.meta_description || 'Prismic Description Here',
+    title: settings.data.site_title || "Prismic Use",
+    description: settings.data.meta_description || "Prismic Description Here",
     openGraph: {
-      images: [settings.data.og_image.url || ''],
+      images: [settings.data.og_image.url || ""],
     },
-  }
+  };
 }
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={clsx(nunito.variable, nunitoSans.variable)}> {/** we may use => className={`${inter.variable} ${roboto_mono.variable}`} */}
+    <html lang="en" className={clsx(nunito.variable, nunitoSans.variable)}>
+      {" "}
+      {/** we may use => className={`${inter.variable} ${roboto_mono.variable}`} */}
       <body>
         <Header />
         {children}
         <Footer />
-        <div className='fixed bg-gradient-to-t from-emerald-50 to-cyan-50 z-[-1] inset-0 opacity-50'></div>
+        <div className="fixed bg-gradient-to-t from-emerald-50 to-cyan-50 z-[-1] inset-0 opacity-50"></div>
         <PrismicPreview repositoryName={repositoryName} />
-      </body> {/** incase single direclty use => inter.className */}
+        <SpeedInsights />
+      </body>{" "}
+      {/** incase single direclty use => inter.className */}
     </html>
-  )
+  );
 }
